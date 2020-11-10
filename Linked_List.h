26c8904f3584de9f88aee024de7be27cc521273b
Anna Rabynovych <57673883+AnnaShw@users.noreply.github.com>
2020-11-10 20:27:02 +0200

    Add files via upload
    Initial commit

---
+++ Linked_List.h
@@ -0,0 +1,19 @@
+#ifndef _LINKED_LIST
+#define _LINKED_LIST
+//#define SWAP(x,y,t)((t)=(x),(x)=(y),(y)=(t))
+typedef struct List list;
+struct List {
+	int data;
+	list* next;
+};
+
+list* BuildList();
+void PrintList(list*);
+list* BuildNode(int);
+list* addToHead(list*, int);
+list* addToTail(list*, int);
+list* addToIndex(list*, int, int);
+void SelectionSort(list*);
+list* FreeList(list*);
+list* Delete(list*, int);
+#endif
